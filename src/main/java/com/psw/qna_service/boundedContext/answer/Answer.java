692258362f208e4e546bf5bd1ae7d0caba42d7da
SangWon7242 <gotkdqja@gmail.com>
2024-11-13 17:29:31 +0900

    Answer 클래스에 question 속성 추가 후 @ManyToOne 적용

--- src/main/java/com/psw/qna_service/boundedContext/answer/Answer.java
+++ src/main/java/com/psw/qna_service/boundedContext/answer/Answer.java
@@ -20,8 +20,6 @@ public class Answer {
 
   private LocalDateTime createDate; // DATETIME
 
-  private Integer questionId;
-
-//  @ManyToOne
-//  private Question question;
+  @ManyToOne
+  private Question question;
 }
