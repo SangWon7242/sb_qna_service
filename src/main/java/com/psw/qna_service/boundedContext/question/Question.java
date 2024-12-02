aaaf99220c30b43c8ed03631e970ef5fa2f24264
SangWon7242 <gotkdqja@gmail.com>
2024-12-02 23:26:18 +0900

    @OneToMany 필드 추가

--- src/main/java/com/psw/qna_service/boundedContext/question/Question.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/Question.java
@@ -1,10 +1,12 @@
 package com.psw.qna_service.boundedContext.question;
 
+import com.psw.qna_service.boundedContext.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,4 +23,12 @@ public class Question {
   private String content;
 
   private LocalDateTime createDate; // DATETIME
+
+  // @OneToMany 자바세상에서의 편의를 위해서 필드 생성
+  // 이 녀석은 실제 DB 테이블에 칼럼이 생성되지 않는다.
+  // DB는 리스트나 배열을 저장할 수 없다.
+  // 만들어도 되고 만들지 않아도 된다.
+  // 다만 만들면 해당 객체(질문객체)에서 관련된 답변들을 찾을 때 편하다.
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
 }
