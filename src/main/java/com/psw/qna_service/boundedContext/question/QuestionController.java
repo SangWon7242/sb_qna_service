3fdaaafca90f7fa4450e44ef0a8a592b5fb15101
SangWon7242 <gotkdqja@gmail.com>
2024-12-18 02:10:11 +0900

    url 프리픽스

--- src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
@@ -1,27 +1,28 @@
 package com.psw.qna_service.boundedContext.question;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
   private final QuestionService questionService;
 
-  @GetMapping("/question/list")
+  @GetMapping("/list")
   public String list(Model model) {
     List<Question> questionList = questionService.findAll();
     model.addAttribute("questionList", questionList);
     return "question_list";
   }
 
-  @GetMapping(value = "/question/detail/{id}")
+  @GetMapping(value = "/detail/{id}")
   public String detail(Model model, @PathVariable("id") Integer id) {
     Question question = questionService.getQuestion(id);
 
@@ -30,3 +31,4 @@ public class QuestionController {
     return "question_detail";
   }
 }
+
