61a2f3fb6edee6ab3709f4260a82db6ec3fe1cd2
SangWon7242 <gotkdqja@gmail.com>
2024-12-18 01:39:47 +0900

    타임리프 적용

---
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
@@ -0,0 +1,12 @@
+package com.psw.qna_service.boundedContext.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuestionController {
+  @GetMapping("/question/list")
+  public String list() {
+    return "question_list";
+  }
+}
