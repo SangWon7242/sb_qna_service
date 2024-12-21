5f5e192cd99b451d038764b9d21eb73315208916
SangWon7242 <gotkdqja@gmail.com>
2024-12-22 02:36:39 +0900

    답변 저장하기

--- src/main/java/com/psw/qna_service/boundedContext/answer/AnswerController.java
+++ src/main/java/com/psw/qna_service/boundedContext/answer/AnswerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
   private final QuestionService questionService;
+  private final AnswerService answerService;
 
   @PostMapping("/create/{id}")
   public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
@@ -23,7 +24,11 @@ public class AnswerController {
     Question question = questionService.getQuestion(id);
 
     // TODO: 답변을 저장한다.
-    // return "redirect:/question/detail/%s".formatted(id);
-    return "question_detail";
+    Answer answer = answerService.create(question, content);
+    // 아래와 같이 처리요청에 대해서는, 처리 후 빨리 떠나는 게 좋다.
+    // 단 적절한 if 문을 사용하면 유용한 방법이 될 수 도 있다. 즉 머무는게 좋은 경우도 있음
+    // return "%d번 질문에 대한 답변이 생성되었습니다.(답변번호 : %d)".formatted(id, answer.getId());
+
+    return "redirect:/question/detail/%s".formatted(id);
   }
 }
