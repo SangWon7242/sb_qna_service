a66a06d40fa8af55d830763d1a273629d76f9d8f
SangWon7242 <gotkdqja@gmail.com>
2024-12-22 03:19:25 +0900

    QuestionForm에 유효성 체크 적용

--- src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.psw.qna_service.boundedContext.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,28 +39,11 @@ public class QuestionController {
   }
 
   @PostMapping("/create")
-  public String questionCreate(QuestionForm questionForm) {
+  // @Valid QuestionForm questionForm
+  // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
+  public String questionCreate(@Valid QuestionForm questionForm) {
+    questionService.create(questionForm.getSubject(), questionForm.getContent());
 
-    String subject = questionForm.getSubject();
-    String content = questionForm.getContent();
-
-    if ( subject == null || subject.trim().isEmpty()) {
-      throw new RuntimeException("subject(을)를 입력해주세요.");
-    }
-
-    if ( subject.trim().length() > 200 ) {
-      throw new RuntimeException("subject(을)를 200자 이하로 입력해주세요.");
-    }
-
-    if ( content == null || content.trim().isEmpty()) {
-      throw new RuntimeException("content(을)를 입력해주세요.");
-    }
-
-    if ( content.trim().length() > 20_000 ) {
-      throw new RuntimeException("content(을)를 20,000자 이하로 입력해주세요.");
-    }
-
-    questionService.create(subject, content);
     return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
   }
 }
