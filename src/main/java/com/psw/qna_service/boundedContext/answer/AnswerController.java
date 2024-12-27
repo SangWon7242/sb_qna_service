49c0d93439ccd8f644b639a9488b6b30b9989feb
SangWon7242 <gotkdqja@gmail.com>
2024-12-27 09:40:05 +0900

    답변등록에 폼 유효성 체크 적용

--- src/main/java/com/psw/qna_service/boundedContext/answer/AnswerController.java
+++ src/main/java/com/psw/qna_service/boundedContext/answer/AnswerController.java
@@ -1,10 +1,12 @@
 package com.psw.qna_service.boundedContext.answer;
 
-import ch.qos.logback.core.model.Model;
 import com.psw.qna_service.boundedContext.question.Question;
 import com.psw.qna_service.boundedContext.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,17 @@ public class AnswerController {
   private final AnswerService answerService;
 
   @PostMapping("/create/{id}")
-  public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+  public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content, @Valid AnswerForm answerForm, BindingResult bindingResult) {
     // 관련 질문을 얻어온다.
     Question question = questionService.getQuestion(id);
 
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("question", question);
+      return "question_detail";
+    }
+
     // TODO: 답변을 저장한다.
-    Answer answer = answerService.create(question, content);
+    Answer answer = answerService.create(question, answerForm.getContent());
     // 아래와 같이 처리요청에 대해서는, 처리 후 빨리 떠나는 게 좋다.
     // 단 적절한 if 문을 사용하면 유용한 방법이 될 수 도 있다. 즉 머무는게 좋은 경우도 있음
     // return "%d번 질문에 대한 답변이 생성되었습니다.(답변번호 : %d)".formatted(id, answer.getId());
