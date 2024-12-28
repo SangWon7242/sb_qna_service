09dcf3e1330120636a18a08921307dd1a01088e3
SangWon7242 <gotkdqja@gmail.com>
2024-12-29 00:10:13 +0900

    회원가입 폼, 처리

--- src/main/java/com/psw/qna_service/config/SecurityConfig.java
+++ src/main/java/com/psw/qna_service/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,5 +14,13 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.build();
+  }
+
+  @Bean
+  // 비밀번호 암호화 검증
+  // 인증 시 암호화된 비밀번호와 비교
+  // BCrypt는 암호화 알고리즘으로 비밀번호 보안성을 향상 시킴
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
