f80aed68246773f7edd8e596a4024b92f87421b5
SangWon7242 <gotkdqja@gmail.com>
2025-01-05 02:07:45 +0900

    답변 수정

--- src/main/java/com/psw/qna_service/boundedContext/answer/AnswerRepository.java
+++ src/main/java/com/psw/qna_service/boundedContext/answer/AnswerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
   @Modifying // 아래 Query는 INSERT, UPDATE, DELETE와 같은 데이터 변경 작업에만 사용해야 한다.
   // nativeQuery = true 여야 MySQL 쿼리 사용이 가능하다.
   @Transactional
