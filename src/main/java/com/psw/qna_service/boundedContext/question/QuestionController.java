8460f92c0f3b5e19f81c49d0deb281876a1cb4d2
SangWon7242 <gotkdqja@gmail.com>
2025-01-09 22:52:13 +0900

    검색

--- src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionController.java
@@ -24,8 +24,8 @@ public class QuestionController {
   private final UserService userService;
 
   @GetMapping("/list")
-  public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
-    Page<Question> paging = questionService.getList(page);
+  public String list(Model model, @RequestParam(defaultValue="0") int page) {
+    Page<Question> paging = questionService.getList(page, null);
 
     model.addAttribute("paging", paging);
     return "question_list";
--- src/main/java/com/psw/qna_service/boundedContext/question/QuestionService.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionService.java
@@ -1,15 +1,16 @@
 package com.psw.qna_service.boundedContext.question;
 
+import com.psw.qna_service.boundedContext.answer.Answer;
 import com.psw.qna_service.boundedContext.user.SiteUser;
 import com.psw.qna_service.boundedContext.user.UserRepository;
 import com.psw.qna_service.exception.DataNotFoundException;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -47,12 +48,31 @@ public class QuestionService {
     return q;
   }
 
-  public Page<Question> getList(int page) {
+  public Page<Question> getList(int page, String kw) {
     List<Sort.Order> sorts = new ArrayList<>();
     sorts.add(Sort.Order.desc("createDate"));
-
     Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-    return questionRepository.findAll(pageable);
+
+    Specification<Question> spec = search(kw);
+    return questionRepository.findAll(spec, pageable);
+  }
+
+  private Specification<Question> search(String kw) {
+    return new Specification<>() {
+      private static final long serialVersionUID = 1L;
+      @Override
+      public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        query.distinct(true);  // 중복을 제거
+        Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+        Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+        Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+        return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+            cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+            cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+            cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+            cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+      }
+    };
   }
 
   public void modify(Question question, String subject, String content) {
@@ -70,4 +90,5 @@ public class QuestionService {
     question.addVoter(voter);
     questionRepository.save(question);
   }
+
 }
