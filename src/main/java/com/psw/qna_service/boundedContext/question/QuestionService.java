31e5374bde56b6c81aabca6f17d8c732bef36780
SangWon7242 <gotkdqja@gmail.com>
2025-01-09 23:13:02 +0900

    검색 폼

--- src/main/java/com/psw/qna_service/boundedContext/question/QuestionService.java
+++ src/main/java/com/psw/qna_service/boundedContext/question/QuestionService.java
@@ -52,6 +52,11 @@ public class QuestionService {
     List<Sort.Order> sorts = new ArrayList<>();
     sorts.add(Sort.Order.desc("createDate"));
     Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+    
+    // 검색어가 없는 경우 유효성 검사
+    if ( kw == null || kw.trim().isEmpty()) {
+      return questionRepository.findAll(pageable);
+    }
 
     Specification<Question> spec = search(kw);
     return questionRepository.findAll(spec, pageable);
